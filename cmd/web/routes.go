7a901abb921bddcd7df5c1c385025ad6baf54642
umar006 <umaralfaruq597@gmail.com>
2022-08-14 19:45:35 +0700

    Add middleware to log HTTP requests
    Use information logger to record the IP address, URL, and method are
    being requested

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,5 +17,5 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
