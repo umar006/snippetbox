ad62a831f34de7c046cf69ae4a44b918b4034975
umar006 <umaralfaruq597@gmail.com>
2022-08-15 09:49:54 +0700

    Update the pattern for the route for static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,7 +18,7 @@ func (app *application) routes() http.Handler {
 	})
 
 	fileServer := http.FileServer(http.Dir(*staticDir))
-	router.Handler(http.MethodGet, "/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreate)
