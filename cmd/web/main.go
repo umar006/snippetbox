2ca4ebead36545e78e5dfbc4deee58486d330a78
umar006 <umaralfaruq597@gmail.com>
2022-08-16 10:11:45 +0700

    Change manual form parsing to automatic form parsing
    Use go-playground/form package to automatically decode the form data
    into createSnippetForm struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 
 	"snippetbox.umaralfaruq/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,6 +19,7 @@ type application struct {
 	infoLog       *log.Logger
 	snippet       *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -39,11 +41,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippet:       &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	serve := &http.Server{
