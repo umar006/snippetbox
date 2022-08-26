d581915d3955852f8436cc3077fb64b86dc77165
umar006 <umaralfaruq597@gmail.com>
2022-08-26 11:03:15 +0700

    Rename user method from Exist to Exists

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -76,7 +76,7 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 			return
 		}
 
-		exists, err := app.users.Exist(id)
+		exists, err := app.users.Exists(id)
 		if err != nil {
 			app.serverError(w, err)
 			return
