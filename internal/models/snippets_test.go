779a9ab2e721e0cd5b38e8cb8950b0f83ae4bf6c
umar006 <umaralfaruq597@gmail.com>
2022-08-28 20:43:51 +0700

    Add integration test for snippet model Get error check

--- internal/models/snippets_test.go
+++ internal/models/snippets_test.go
@@ -31,3 +31,44 @@ func TestSnippetModelGet(t *testing.T) {
 		assert.NilError(t, err)
 	})
 }
+
+func TestSnippetModelGetError(t *testing.T) {
+	if testing.Short() {
+		t.Skip("models: skipping integration test")
+	}
+
+	tests := []struct {
+		name      string
+		snippetID int
+		wantError error
+	}{
+		{
+			name:      "Zero ID",
+			snippetID: 0,
+			wantError: ErrNoRecord,
+		},
+		{
+			name:      "Negative ID",
+			snippetID: -1,
+			wantError: ErrNoRecord,
+		},
+		{
+			name:      "Non-existent ID",
+			snippetID: 5,
+			wantError: ErrNoRecord,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			db := newTestDB(t)
+
+			m := SnippetModel{db}
+
+			snippet, err := m.Get(test.snippetID)
+
+			assert.EqualError(t, err, test.wantError)
+			assert.Equal(t, snippet, nil)
+		})
+	}
+}
