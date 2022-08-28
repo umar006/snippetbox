6b1071d7a51473b9947ef848ebece74a38406562
umar006 <umaralfaruq597@gmail.com>
2022-08-28 10:30:32 +0700

    Create integration test for usermodel exists method

---
+++ internal/models/users_test.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"testing"
+
+	"snippetbox.umaralfaruq/internal/assert"
+)
+
+func TestUserModelExists(t *testing.T) {
+	if testing.Short() {
+		t.Skip("models: skipping integration test")
+	}
+
+	tests := []struct {
+		name   string
+		userID int
+		want   bool
+	}{
+		{
+			name:   "Valid ID",
+			userID: 1,
+			want:   true,
+		},
+		{
+			name:   "Zero ID",
+			userID: 0,
+			want:   false,
+		},
+		{
+			name:   "Non-existent ID",
+			userID: 2,
+			want:   false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			db := newTestDB(t)
+
+			m := UserModel{db}
+
+			exists, err := m.Exists(test.userID)
+
+			assert.Equal(t, exists, test.want)
+			assert.NilError(t, err)
+		})
+	}
+}
